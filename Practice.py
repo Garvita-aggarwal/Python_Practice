7a2f703ffaf8f41b4f15081a1939a33efa755bed
Garvita Aggarwal <garvitaa18@gmail.com>
2026-09-14 00:03:17 +0530

    Learned variables, Datatypes and I/O

--- Practice.py
+++ Practice.py
@@ -1,3 +1,18 @@
+# Variables (Used to stor values)
+
+name = "Yashu" 
+age  =  22
+weight = 62.0 
+
+
+# Datatypes
+
+name = "Yashu" #String
+age  =  22     # Int = Only whole nummbers
+weight = 62.0  # Boolean = whole no + decimals
+
+
+# Input/output
 name = input("Enter name : ");
 
 print("Name :",name);
