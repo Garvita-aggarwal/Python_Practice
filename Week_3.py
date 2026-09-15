63bcac5b605579adc78bb7b0fc33c7cc39ddc292
Garvita Aggarwal <garvitaa18@gmail.com>
2026-09-15 23:51:03 +0530

    Intial Commit

---
+++ Week_3.py
@@ -0,0 +1,18 @@
+# Functions
+#Defining it
+
+def greet(name):
+    return f"Hello,{name} !"
+print(greet("AI"))
+
+
+#Parameters & arguments
+#return
+#Multiple parameters
+#Default arguments
+#Keyword arguments
+#Local vs global variables
+#*args
+#**kwargs
+#Lambda functions
+
