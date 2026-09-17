daec15917a6dfdec79690ed0cc21a2d47aab0089
Garvita Aggarwal <garvitaa18@gmail.com>
2026-09-17 23:56:22 +0530

    Minor Changes

--- Week_3.py
+++ Week_3.py
@@ -1,13 +1,22 @@
 # Functions
-#Defining it
+#Defining it (A function is a reusable block of code designed to perform a specific task.)
 
 def greet(name):
     return f"Hello,{name} !"
 print(greet("AI"))
 
 
-#Parameters & arguments
-#return
+
+#Part	Meaning
+#def	      Keyword used to define a function
+#greet	      Function name
+#()	          Parameters go here
+#:	          Starts the function body
+#Indented code	Function body
+
+
+
+
 #Multiple parameters
 #Default arguments
 #Keyword arguments
@@ -16,3 +25,16 @@ print(greet("AI"))
 #**kwargs
 #Lambda functions
 
+
+#2. Strings
+#Indexing & slicing
+#upper(), lower(), capitalize()
+#strip()
+#replace()
+#split()
+#join()
+#find()
+#count()
+#startswith() / endswith()
+#f-strings
+#String formatting
